35ba0e69814c1fb41059ffdebbac3cc2a3f197ae
Yizhang LIN <linyizh@berkeley.edu>
2020-08-13 11:19:12 -0700

    remove redundant scripts

--- inspect_ckpt.py
+++
@@ -1,6 +0,0 @@
-import tensorflow as tf
-from tensorflow.python.tools.inspect_checkpoint import print_tensors_in_checkpoint_file
-
-
-latest_ckp = './checkpoints/unet_flownet2_tr2_best_lr-1'
-print_tensors_in_checkpoint_file(latest_ckp, all_tensors=True, tensor_name='')
